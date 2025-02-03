611ea8886df8517ad999e9268c7809bfcd1cf7af
Marc Reylan <mmadayag@estudiante.edib.es>
2025-02-03 20:21:11 +0100

    Inserción de un código

--- js/main.js
+++ js/main.js
@@ -33,3 +33,28 @@ document.addEventListener("keydown", function(event) {
         window.location.href="https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExYWNjaDcyZHVnaGF6Z29vNnA3d2gyNnFmNHdiMm9va3AzejFkamNvNyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/arXyCBxVhhHG0/giphy.gif";
     }
 });
+
+document.querySelectorAll('.editarTarea').forEach((boton) =>{
+    boton.addEventListener('click', (event) =>{
+        const editContainer = document.querySelector('.editFormContainer');
+        const inputEditar = document.querySelector('.editFormContainer input[name="tarea"]');
+        const identificatorInput = document.querySelector('.editFormContainer form input[name="identificator"]');
+
+        const tareaDiv = event.target.closest('.tarea');
+
+        const tareaTexto = tareaDiv.querySelector('span').textContent;
+
+        const TareaID = tareaDiv.querySelector('form input[name="identificator"]').value;
+
+        inputEditar.value = tareaTexto;
+
+        identificatorInput.value = tareaID;
+
+        editContainer.classList.remove('hidden');
+
+    });
+});
+
+document.querySelector('.editFormContainer .closeContainer').addEventListener('click', () => {
+    document.querySelector('.editFormContainer').classList.add('hidden');
+});
